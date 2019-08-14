f5ebec566d8de7109a605268d8ecdb2724e5ecd3
YoungD <>
2019-08-14 21:17:35 +0900

    first commit

---
+++ SSGHomeWork/SSGHomeWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SSGHomeWork
+//
+//  Created by YoungD on 2019. 8. 14..
+//  Copyright © 2019년 YoungD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
